30367a39e886b4d7f6b558b5558a1d218a100c1d
Ruslan Galiev <ruslan.galiev@outlook.com>
2026-02-01 02:11:23 +0300

    M9: quality - fix Android SA1118

--- src/mobile/TheButton.Mobile/Platforms/Android/MainActivity.cs
+++ src/mobile/TheButton.Mobile/Platforms/Android/MainActivity.cs
@@ -10,12 +10,7 @@ namespace TheButton.Mobile.Platforms.Android;
     Theme = "@style/Maui.SplashTheme",
     MainLauncher = true,
     LaunchMode = LaunchMode.SingleTop,
-    ConfigurationChanges = ConfigChanges.ScreenSize
-        | ConfigChanges.Orientation
-        | ConfigChanges.UiMode
-        | ConfigChanges.ScreenLayout
-        | ConfigChanges.SmallestScreenSize
-        | ConfigChanges.Density)]
+    ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
 }
