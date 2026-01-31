1c243dc06b69f5bb0b01c97e6ee2715e43ced0d5
Ruslan Galiev <ruslan.galiev@outlook.com>
2026-02-01 01:37:43 +0300

    M9: quality - fix Android platform analyzer errors

--- src/mobile/TheButton.Mobile/Platforms/Android/MainApplication.cs
+++ src/mobile/TheButton.Mobile/Platforms/Android/MainApplication.cs
@@ -1,15 +1,27 @@
-﻿using Android.App;
+using Android.App;
 using Android.Runtime;
 
-namespace TheButton.Mobile;
+namespace TheButton.Mobile.Platforms.Android;
 
+/// <summary>
+/// Android application entry point.
+/// </summary>
 [Application]
 public class MainApplication : MauiApplication
 {
-	public MainApplication(IntPtr handle, JniHandleOwnership ownership)
-		: base(handle, ownership)
-	{
-	}
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MainApplication"/> class.
+    /// </summary>
+    /// <param name="handle">The native handle.</param>
+    /// <param name="ownership">The ownership transfer.</param>
+    public MainApplication(IntPtr handle, JniHandleOwnership ownership)
+        : base(handle, ownership)
+    {
+    }
 
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    /// <inheritdoc />
+    protected override MauiApp CreateMauiApp()
+    {
+        return MauiProgram.CreateMauiApp();
+    }
 }
